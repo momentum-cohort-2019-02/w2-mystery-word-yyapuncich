c4c023f073a1b4a2f64905b3d0c536749eb568a5
Yifaa Yapuncich <yifaasy@gmail.com>
2019-02-22 13:20:30 -0500

    made function to ask user for level and checks if valid selection

--- mystery_word.py
+++ mystery_word.py
@@ -1,14 +1,38 @@
 import random
 import string
 
-# Ask user what mode to play in
-user_mode_select = input("What level would you like to play? Easy, Normal, or Hard? ")
+# defining acceptable answers by user for level selection as variable VALID_MODE
+valid_mode = "Easy" or "Normal" or "Hard"
 
-# check if mode is valid
-def check_mode(mode):
-    user_mode_select = user_mode_select.capitalize()
-    if user_mode_select != "Easy" or "Normal" or "Hard":
-        print("invalid selection, please enter valid level: ")
+def get_user_level():
+    """Asks the user for their level preference until they answer in valid format, then returns that level"""
+    # assigning variable ASK to empty string before question is asked
+    ask = ""
+# loop that will ask user for level preference until they answer with valid selection
+    while ask is not valid_mode:
+        ask = input("What level would you like to play? Easy, Normal, or Hard? ").capitalize()
+        if ask == "Easy":
+            user_mode_select = "Easy"
+            return user_mode_select
+        elif ask == "Normal":
+            user_mode_select = "Normal"
+            return user_mode_select
+        elif ask == "Hard":
+            user_mode_select = "Hard"
+            return user_mode_select
+        else:
+            user_mode_select = "Invalid"
+            print("Invalid selection, please try again") 
+        # now return to first question and ask again for valid entry
+
+# assigns variable GAME_MODE to whatever the user selected
+game_mode = get_user_level()
+# print(game_mode) #checking if the above works and it does...
+
+# def normal_mode():
+
+# def hard_mode():
 
 
 
+# easy_mode(user_mode_select)
