eb7107e28094a5900514c4b8ece52ba25728b53e
Yifaa Yapuncich <yifaasy@gmail.com>
2019-02-22 14:30:08 -0500

    got my words to add to a list of easy length words

--- mystery_word.py
+++ mystery_word.py
@@ -26,13 +26,32 @@ def get_user_level():
         # now return to first question and ask again for valid entry
 
 # assigns variable GAME_MODE to whatever the user selected
-game_mode = get_user_level()
+# game_mode = get_user_level()
 # print(game_mode) #checking if the above works and it does...
 
+# creates game that selects random word from list and asks user for guesses
+def mystery_game_easy(word_file):
+    """Comp selects word 4-6 characters long and user guesses letter until word is complete or they run out of guesses"""
+    with open(word_file) as file:
+        words = file.read()
+        file.close
+    # create list of words from txt file of words
+    list_words = []
+    for word in words.split():
+        if len(word) >= 4 and len(word) <= 6:
+            list_words.append(word)
+            
+    return list_words
+    
+easy_words = mystery_game_easy("words.txt")
+    
+
+
+    # print instructions for user
+    # print("You have a word that is 4-6 characters in length. You have 8 attempts to guess letters in the word.")
+    # set variable to get guesses from user, store that letter.
+    # get_guesses = input("Enter a letter:")
+    # while 
 # def normal_mode():
 
 # def hard_mode():
-
-
-
-# easy_mode(user_mode_select)
