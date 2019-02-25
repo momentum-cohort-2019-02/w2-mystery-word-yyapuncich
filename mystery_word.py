d954d4f35f43e1ae6a3a91bb85e914ebbdb4968f
Yifaa Yapuncich <yifaasy@gmail.com>
2019-02-24 21:06:56 -0500

    I finally figured out how to make letters all uppercase

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,6 @@
 import random
 import string
+##### help me understand lambda in list comprehension ##########
 
 
 def get_user_level():
@@ -36,14 +37,14 @@ def get_user_level():
         # now return to first question and ask again for valid entry
 
 # assigns variable GAME_MODE to whatever the user selected
-game_mode = get_user_level()
 # print(game_mode) #checking if the above works and it does...
 
 # creates game that selects random word from list and asks user for guesses
 def list_generator(word_file):
     """Comp selects word 4-6 characters long and user guesses letter until word is complete or they run out of guesses"""
     with open(word_file) as file:
-        words = file.read()
+        words = file.read().upper()
+        
     # create list of words from txt file of words
     # set it up to do normal and hard mode here too, if else statements
     if game_mode == "Easy":
@@ -56,13 +57,12 @@ def list_generator(word_file):
         list_words = [word for word in words.split() if (len(word) > 8)]
         return list_words
 
-selection_list = list_generator("words.txt")
 # print(selection_list) #checking here to see if list selection works and it does
 def get_user_input():
     """Gets a guess from user and makes sure it is only one alpha letter"""
 # don't totally understand how while True works... is it just always true??
     while True:
-        user_input = input("Guess a letter in the mystery word: ")
+        user_input = input("Guess a letter in the mystery word: ").upper()
         if user_input in list(string.ascii_letters):
             guess = str(user_input)
             if len(guess) == 1:     
@@ -85,21 +85,24 @@ def play_game():
     print("You have a word that is 4-6 characters in length. You have 8 attempts to guess letters in the word.")
     # set variable to get guesses from user, store that letter.
     mystery_word = list(random.choice(selection_list))
-    guess = None
+    guess = []
     attempts = 0
     all_guesses = []
-    
+    # letters = list(string.ascii_letters)
+    # convert mystery word to uppercase
     # print(display_list) # testing if it works!
-    while guess != mystery_word and attempts < 8:
+    while mystery_word not in all_guesses and attempts < 8:
         print(mystery_word, "mystery word") # testing if it works!
         guess = get_user_input()
         respond_to_user(mystery_word, guess)
-        attempts += 1
+        if guess not in mystery_word:
+            attempts += 1
          ######### it took me forever to get the following syntax right. could not figure out how to add my guesses to a list!!! ahaha!!!!!
         all_guesses += str(guess)
         display_word(mystery_word, all_guesses)
         # print(all_guesses, "all guesses") # testing if list of guesses works and. it. does.
-        print(f"""This was attempt number {attempts}. You have 8 total attempts. Would you like to play again?""")
+        print(f"""There are {8 - int(attempts)} turns remaining. 
+        Each incorrect guess will deduct from your turns.""")
 
 def display_word(word, guesses):
     # display = [letter if letter in guesses else "_" for letter in word]
@@ -112,6 +115,7 @@ def display_word(word, guesses):
 #     print(output_letters)
             
     #get user input Y/N and let them play again or thank them for playing
-
+game_mode = get_user_level()
+selection_list = list_generator("words.txt")
 play_game()   
     
